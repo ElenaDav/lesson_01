90fdd57f68d8acce05eda9d8febeedc27a6f11db
Elena <hardevich@gmail.com>
2023-04-12 23:00:22 +0400

    002 task

---
+++ Seminar01/002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 числo: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 числo: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == (numberB * numberB))
+  {
+    System.Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+  }
+else
+  {
+    System.Console.WriteLine("Upssss");
+  }
