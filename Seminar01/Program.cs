cade925402b3587d972ae6fdd1c15a90f1e2326f
Elena <hardevich@gmail.com>
2023-04-12 21:55:48 +0400

    seminar01 example01

---
+++ Seminar01/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+
+string s = Console.ReadLine();
+// int i = int.Parse(s); //преобразование строки в число
+int i;
+
+if (int.TryParse(s, out i))
+{
+  System.Console.WriteLine($"Квадрат числа {i} = {Math.Pow(i, 2)}");
+}
+else
+{
+  System.Console.WriteLine("Upssss");
+}
+
+// System.Console.WriteLine("Квадрат числа " + i + " = " + i * i);
+// System.Console.WriteLine("Квадрат числа " + i + " = " + Math.Pow(i, 2));
+// System.Console.WriteLine($"Квадрат числа {i} = {Math.Pow(i, 2)}");
+
