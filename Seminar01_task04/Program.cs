bf12cf523ad7b29ea2bf13c2cbe81c83af74e6af
Elena <hardevich@gmail.com>
2023-04-15 21:54:26 +0400

    add task 04 for seminar 01

---
+++ Seminar01_task04/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите N: ");
+int s = int.Parse(Console.ReadLine());
+
+for (int i = -s; i <= s; i++)
+{
+  System.Console.WriteLine(i);
+}
+
+System.Console.WriteLine("Введите N: ");
+int y = int.Parse(Console.ReadLine());
+if (y >= 100 && y < 1000)
+{
+  System.Console.WriteLine(y % 10);
+}
+else
+{
+  System.Console.WriteLine("Это не трехзначное число");
+}
